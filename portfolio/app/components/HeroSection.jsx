65265c4a5771f03685ed4490cba9283cf6ae0622
lokifc <132177231+lokifc@users.noreply.github.com>
2024-09-04 02:05:02 +0800

    uppp

--- portfolio/app/components/HeroSection.jsx
+++ portfolio/app/components/HeroSection.jsx
@@ -1,5 +1,8 @@
+"use client";
 import  React from "react";
 import Image  from "next/image";
+import { TypeAnimation } from 'react-type-animation';
+
 
 const HeroSection =() =>{
     return(
@@ -7,8 +10,26 @@ const HeroSection =() =>{
         <div className="grid grid-cols-1 sm:grid-cols-12">
             <div className="col-span-7 place-self-center text-center sm:text-left">
                     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold ">
-                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I'm{" "}</span>
-                        Lulu
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                            Hello, I'm{" "}
+                        </span>
+                        <br></br>
+                        <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Lulu',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'Dog Person',
+        1000,
+        'Web Developer',
+        1000,
+        'Data Scientist',
+        1000
+      ]}
+      wrapper="span"
+      speed={40}
+    repeat={Infinity}
+    />
                     </h1>
                         <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
                                   I love dogs
@@ -26,8 +47,8 @@ const HeroSection =() =>{
                     src = "/images/bozyu.jpg"
                     alt = "bozyu"
                     className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                    width={300}
-                    height={300 }
+                    width={250}
+                    height={250}
                     />
                 </div>
             </div>
