f29fd117b8bea68e4eef77ebe0c2e1a3daf77f86
lokifc <132177231+lokifc@users.noreply.github.com>
2024-09-09 18:25:06 +0800

    up

--- portfolio/app/components/Navbar.jsx
+++ portfolio/app/components/Navbar.jsx
@@ -1,13 +1,42 @@
 import React from "react";
+import Link from "next/link";
+import NavLink from "./NavLink";
+
+const navLinks = [
+    {
+        title: "About",
+        path:"#about",
+    },
+    {
+        title: "Projects",
+        path:"#projects",
+    },
+    {
+        title: "Contact",
+        path:"#contact",
+    }
+]
 
 const Navbar =() => {
     return(
         <nav>
-        <div>
+        <div className="flex flex-wrap items-center justify-between mx-auto p-8">
+            <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">
+                LOGO</Link>
+            <div className="menu hidden md:block md:w-auto" id="navbar">
+                <ul className="flex p-4 md:p-0 md:flex-row md: space-x-8 mt-8">
+                    {navLinks.map((link, index) =>(
+                        <li key={index}>
+                            <NavLink href = {link.path} title={link.title} />
+                        </li>
+                    ))    
+                    }
+                </ul>
+            </div>
         </div>
         </nav>
 
     )
 }
 
-export default Navbar
+export default Navbar; 
