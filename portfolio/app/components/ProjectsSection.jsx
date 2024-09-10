41df772a8144e997549f99ba04f32a2bedbc7c44
lokifc <132177231+lokifc@users.noreply.github.com>
2024-09-11 00:36:22 +0800

    update2

---
+++ portfolio/app/components/ProjectsSection.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+const projectData = [
+    {
+    id: 1,
+    title:"Loki shower",
+    description: "Loki's first time shower",
+    image: "/images/projects/1.jpg",
+    tag: ["All", "Lulu","Loki"],
+},
+{
+    id: 2,
+    title:"Happy",
+    description: "Happy the golden retriever",
+    image: "/images/projects/2.jpg",
+    tag: ["All", "Happy", "Golden Retriever"],
+},
+{
+    id: 3,
+    title:"Loki cool",
+    description: "Young Loki",
+    image: "/images/projects/3.jpg",
+    tag: ["All", "Lulu", "Loki"],
+},
+{
+    
+        id: 4,
+        title:"Bozyu Bone",
+        description: "Bozyu and the bone",
+        image: "/images/projects/4.jpg",
+        tag: ["All", "Lulu", "Bozyu", "Border Collie"],
+},
+{
+    
+    id: 5,
+    title:"Fifi",
+    description: "Fifi is smelly",
+    image: "/images/projects/5.jpg",
+    tag: ["All", "Fifi"],
+},{
+    
+    id: 6,
+    title:"Bozyu Ice cream",
+    description: "Bozyu and the ice cream",
+    image: "/images/projects/6.jpg",
+    tag: ["All", "Lulu", "Bozyu", "Border Collie"],
+},{
+    
+    id: 7,
+    title:"Bozyu frisbee",
+    description: "Bozyu and the frisbee",
+    image: "/images/projects/7.jpg",
+    tag: ["All", "Lulu", "Bozyu", "Border Collie"],
+},{
+    
+    id: 8,
+    title:"Bozyu",
+    description: "Bozyu thinks you're idiot",
+    image: "/images/projects/8.jpg",
+    tag: ["All", "Lulu", "Bozyu", "Border Collie"],
+}, {
+    
+    id: 9,
+    title:"Luna",
+    description: "Cute Luna",
+    image: "/images/projects/9.HEIC",
+    tag: ["All", "Lulu", "Luna", "Border Collie"],
+},
+]
+
+const ProjectsSection = () => {
+    return(
+        <><h2>Projects</h2>
+        <div></div>
+        </>
+    )
+};
+
+export default ProjectsSection;
