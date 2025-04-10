2f860260fd49eed2999f819f51aea991817153fa
lokifc <132177231+lokifc@users.noreply.github.com>
2025-04-11 05:36:13 +0800

    image

--- portfolio/app/components/HeroSection.jsx
+++ portfolio/app/components/HeroSection.jsx
@@ -45,7 +45,7 @@ const HeroSection =() =>{
             </div>
             <div className="col-span-5 place-self-center mt-4 lg:mt-0">
                 <div className="rounded-full  w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-                <Image
+                <img
                     src = "./images/projects/bozyu.jpg"
                     alt = "bozyu"
                     className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
