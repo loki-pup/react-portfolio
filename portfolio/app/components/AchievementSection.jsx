dc436841fccede448a59ec25e58dcc94c568fc2e
lokifc <132177231+lokifc@users.noreply.github.com>
2025-04-12 04:24:21 +0800

    up

--- portfolio/app/components/AchievementSection.jsx
+++ portfolio/app/components/AchievementSection.jsx
@@ -32,7 +32,7 @@ const achievementList = [
 const AchievementSection = () =>{
     return (
         <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-4 ">
-            <div className=" sm:border-slate-500/40 sm:border rounded-md py-8 px-16 sm:px-10 flex flex-col gap-5 sm:flex-row items-center justify-between">
+            <div className=" sm:border-slate-500/40 sm:border rounded-md py-8 sm:py-6 px-16 sm:px-10 flex flex-col gap-5 sm:flex-row items-center justify-between">
             {achievementList.map((achievement, index) => {
                 return (
                     <div 
--- portfolio/app/components/HeroSection.jsx
+++ portfolio/app/components/HeroSection.jsx
@@ -2,13 +2,19 @@
 import  React from "react";
 import Image  from "next/image";
 import { TypeAnimation } from 'react-type-animation';
+import { motion } from "framer-motion";
 
 
 const HeroSection =() =>{
     return(
-        <section className="lg:py-16">
+        <section className="lg:mb-12 ">
         <div className="grid grid-cols-1 sm:grid-cols-12">
-            <div className="col-span-7 place-self-center text-center sm:text-left justify-self-start md:pl-8">
+            <motion.div 
+            initial={{opacity:0.3, scale:0.5}}
+            animate={{opacity:1, scale:1}}
+            transition={{duration:0.5}}
+            className="col-span-7 place-self-center text-center sm:text-left justify-self-start md:pl-8"
+            >
                     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold ">
                         <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-purple-600">
                             Hello, I&#39;m{" "}
@@ -48,13 +54,17 @@ const HeroSection =() =>{
                                 link.click();
                                 document.body.removeChild(link);
                               }}
-                            className="px-1 py-1 w-full sm:w-fit rounded-full  bg-gradient-to-br from-amber-500 via-red-500 to-purple-500 hover:bg-slate-800 text-white mt-3">
+                            className="px-1 py-1 w-full sm:w-fit rounded-full  bg-gradient-to-br from-amber-500 via-red-500 to-purple-500 hover:bg-slate-800 text-white mt-3 mb-6 sm:my-0">
                                 <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                                     Download Bozyu</span>
                                 </button>
                         </div>
-            </div>
-            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+            </motion.div>
+            <motion.div 
+            initial={{opacity:0.3, scale:0.5}}
+            animate={{opacity:1, scale:1}}
+            transition={{duration:0.5}}
+             className="col-span-5 place-self-center mt-4 lg:mt-0">
                 <div className="rounded-full  w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                 <img
                     src = "./images/bozyu.jpg"
@@ -64,7 +74,7 @@ const HeroSection =() =>{
                     height={280}
                     />
                 </div>
-            </div>
+            </motion.div>
         </div>
         </section>
     );
