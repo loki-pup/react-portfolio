08cc161b0a02875dba9350047061af18150c3033
lokifc <132177231+lokifc@users.noreply.github.com>
2025-04-11 08:32:54 +0800

    uo

--- portfolio/app/components/HeroSection.jsx
+++ portfolio/app/components/HeroSection.jsx
@@ -35,9 +35,20 @@ const HeroSection =() =>{
                                   I love food
                         </p>
                         <div>
-                            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-amber-500 via-red-500 to-purple-500  hover:bg-slate-200 text-white">
+                            <button
+                            onClick={() => window.open("https://www.paypal.com/paypalme/bobozyu?locale.x=en_AU", "_blank")} 
+                            className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-amber-500 via-red-500 to-purple-500  hover:bg-slate-200 text-white">
                                 Feed Me</button>
-                            <button className="px-1 py-1 w-full sm:w-fit rounded-full  bg-gradient-to-br from-amber-500 via-red-500 to-purple-500 hover:bg-slate-800 text-white mt-3">
+                            <button 
+                                onClick={() => {
+                                const link = document.createElement("a");
+                                link.href = "./images/projects/8.jpg"; // Make sure it's in /public
+                                link.download = "bozyu.jpg";
+                                document.body.appendChild(link);
+                                link.click();
+                                document.body.removeChild(link);
+                              }}
+                            className="px-1 py-1 w-full sm:w-fit rounded-full  bg-gradient-to-br from-amber-500 via-red-500 to-purple-500 hover:bg-slate-800 text-white mt-3">
                                 <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                                     Download Bozyu</span>
                                 </button>
@@ -46,7 +57,7 @@ const HeroSection =() =>{
             <div className="col-span-5 place-self-center mt-4 lg:mt-0">
                 <div className="rounded-full  w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                 <img
-                    src = "./images/projects/bozyu.jpg"
+                    src = "./images/bozyu.jpg"
                     alt = "bozyu"
                     className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                     width={280}
